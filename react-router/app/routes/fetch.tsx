9be9628cbaf4d56a2d48987ad335fb7ffc08eab9
Jonathan Sedrez <jonathansedrez@gmail.com>
2026-02-04 18:24:47 -0300

    feat(react-route): add both approaches to access laoder data

--- react-router/app/routes/fetch.tsx
+++ react-router/app/routes/fetch.tsx
@@ -1,29 +1,38 @@
-import { useRevalidator } from "react-router";
+import { useLoaderData, useRevalidator } from "react-router";
 import type { Route } from "./+types/fetch";
+
+type User = {
+  name: string;
+  email: string;
+  company: {
+    name: string;
+  };
+};
 
 export async function clientLoader() {
   const userId = Math.floor(Math.random() * 10) + 1;
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/users/${userId}`,
   );
-  const user = await response.json();
+  const user: User = await response.json();
   return { user };
 }
 
 export default function Fetch({ loaderData }: Route.ComponentProps) {
   const revalidator = useRevalidator();
+  const { user } = useLoaderData<typeof clientLoader>();
 
   return (
     <div>
       <h2>Fetched User Data</h2>
       <p>
-        <strong>Name:</strong> {loaderData.user.name}
+        <strong>Name (from prop):</strong> {loaderData.user.name}
       </p>
       <p>
-        <strong>Email:</strong> {loaderData.user.email}
+        <strong>Email (from prop):</strong> {loaderData.user.email}
       </p>
       <p>
-        <strong>Company:</strong> {loaderData.user.company.name}
+        <strong>Company (from hook):</strong> {user.company.name}
       </p>
       <button
         onClick={() => revalidator.revalidate()}
